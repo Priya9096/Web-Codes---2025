0d05a8d9f1328edb3d45d2d7aa88f4a8985ce445
Priya9096 <priyakhairate0000@gmail.com>
2025-05-10 10:41:27 +0530

    added

---
+++ JS NOTES.js
@@ -0,0 +1,12 @@
+/*
+1. Why JavaScript?
+JS is used to add Interactivity to Apps.
+Apps will listen to user events and apps will
+respond accordingly.
+
+2. Version: ES6 (Ecma Script 6)
+
+3.Style of JS : Object oriented, scripting, procedural
+
+4.Brendan Eich in 10 Days he invented JS.
+*/
